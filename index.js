0cd65c16b1c19e60af7be2100b31adbbfb451bb7
Thooto <theopublic@neuf.fr>
2018-06-03 19:35:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+let app = express();
+require('./lib/config')(app);
+require('./lib/routes')(app);
+
+mongoose.connect('mongodb://cmsapp:cmspwd5734@ds159509.mlab.com:59509/cmsdb', (err) => {
+    if (err) throw err;
+    else console.log('database connected');
+});
+
+app.listen(80, (err) => {
+    if (err) throw err;
+    else console.log('server started');
+});
