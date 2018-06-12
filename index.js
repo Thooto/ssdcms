545ee48f7a5ac9fd27c58a81c5bbdf39890d1699
Thooto <theopublic@neuf.fr>
2018-06-13 01:34:09 +0200

    added login system using passport (maybe not definitive)

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-// Require express, http server framework
 const express = require('express');
-// Require mongoose, mongodb framework
 const mongoose = require('mongoose');
+const passport = require('passport');
 
-// App initialisation
 const app = express();
+require('./lib/config')(app, passport);
+require('./lib/routes')(app, passport);
 
-// App configuration
-require('./lib/config')(app, express);
 
-// App routes setup
-require('./lib/routes')(app);
-
-// Connect to the database with mongoose
 mongoose.connect('mongodb://cmsapp:cmspwd5734@ds159509.mlab.com:59509/cmsdb', (err) => {
     if (err) throw err;
     console.log('database connected');
 });
 
-// Start the server on port 80
+
 app.listen(80, (err) => {
     if (err) throw err;
     console.log('server started');
