5633f51563369e606a1159f7a30dc9c33df374fe
Thooto <theopublic@neuf.fr>
2018-06-13 14:55:48 +0200

    override

---
+++ README.md
@@ -0,0 +1,14 @@
+# ssdcms
+Conference Management System for SSD course
+
+How to proceed:
+
+have git and nodejs installed, open terminal
+
+clone the project when you are in some folder (type: git clone https://www.github.com/Thooto/ssdcms.git)
+
+then go to the newly created folder (type: cd ssdcms)
+
+then install dependencies (type: npm install)
+
+then start the server (type: node index.js)
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const passport = require('passport');
+
+const app = express();
+require('./lib/config')(app); //, passport);
+require('./lib/routes')(app); //, passport);
+
+app.use(express.static('lib/assets'));
+
+
+mongoose.connect('mongodb://cmsapp:cmspwd5734@ds159509.mlab.com:59509/cmsdb', (err) => {
+    if (err) throw err;
+    console.log('database connected');
+});
+
+
+app.listen(80, (err) => {
+    if (err) throw err;
+    console.log('server started');
+});
