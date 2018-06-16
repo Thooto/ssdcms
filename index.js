f5bbd55c883222ec40e4e5c06ae647717002c4ee
Thooto <theopublic@neuf.fr>
2018-06-16 14:06:24 +0200

    details finished send submission small fix

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-// const passport = require('passport');
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express();
-require('./lib/config')(app); //, passport);
-require('./lib/routes')(app); //, passport);
 
-app.use(express.static('lib/assets'));
+const app = express()
+require('./lib/config')(app)
+require('./lib/routes')(app)
+
+app.use(express.static('lib/assets'))
 
 
 mongoose.connect('mongodb://cmsapp:cmspwd5734@ds159509.mlab.com:59509/cmsdb', (err) => {
-    if (err) throw err;
-    console.log('database connected');
-});
+    if (err) throw err
+    console.log('database connected')
+})
 
 
-app.listen(8080, (err) => {
-    if (err) throw err;
-    console.log('server started');
-});
+app.listen(80, (err) => {
+    if (err) throw err
+    console.log('server started')
+})
