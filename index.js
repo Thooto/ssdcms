4ff90ad94a8861498bdf018196706c3e0a97fc7d
Thooto <theopublic@neuf.fr>
2018-06-18 21:14:08 +0200

    made more air (double returns to the line)

--- index.js
+++ index.js
@@ -9,20 +9,24 @@ asynchronous capabilities through async and await keywords.
 continuing to run the code while the callback is running asynchronously
 */
 
+
 // Require express and mongoose
 const express = require('express')
 const mongoose = require('mongoose')
+
 
 // Creating and configuring the application
 const app = express()
 require('./lib/config')(app)
 require('./lib/routes')(app)
 
+
 // Connect the server to the database server
 mongoose.connect('mongodb://cmsapp:cmspwd5734@ds159509.mlab.com:59509/cmsdb', (err) => {
     if (err) throw err
     console.log('database connected')
 })
+
 
 // Start the server on port 80(80)
 app.listen(8080, (err) => {
